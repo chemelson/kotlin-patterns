8d82359c78e70fb56f319691a7165d647941d079
chemelson <ross03@mail.ru>
2024-05-31 08:47:07 +0300

    Implement Decorator pattern

---
+++ src/main/kotlin/ru/chemelson/patterns/gof/decorator.kt
@@ -0,0 +1,43 @@
+package ru.chemelson.patterns.gof
+
+fun main() {
+    var pizza: Pizza = PlainPizza()
+    pizza = CheeseDecorator(pizza)
+    pizza = PepperoniDecorator(pizza)
+
+    println("Cost: " + pizza.cost)
+    println("Description: " + pizza.description)
+}
+
+// Abstract component
+interface Pizza {
+    val cost: Double
+    val description: String
+}
+
+// Concrete component
+class PlainPizza : Pizza {
+    override val cost: Double
+        get() = 10.0
+
+    override val description: String
+        get() = "Plain Pizza"
+}
+
+// First decorator
+class CheeseDecorator(private val pizza: Pizza) : Pizza {
+    override val cost: Double
+        get() = pizza.cost + 2.5
+
+    override val description: String
+        get() = "${pizza.description}, Cheese"
+}
+
+// Second decorator
+class PepperoniDecorator(private val pizza: Pizza) : Pizza {
+    override val cost: Double
+        get() = pizza.cost + 3
+
+    override val description: String
+        get() = "${pizza.description}, Pepperoni"
+}
